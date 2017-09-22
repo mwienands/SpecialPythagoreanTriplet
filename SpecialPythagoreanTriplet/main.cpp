e1a6a628fdf5ca51346ad8cfee19ddb996f1e184
mwienands <mwienands@gmail.com>
2017-09-22 18:21:53 -0500

    Init commit

--- SpecialPythagoreanTriplet/main.cpp
+++ SpecialPythagoreanTriplet/main.cpp
@@ -4,12 +4,22 @@
 //
 //  Created by Michael Wienands on 9/22/17.
 //  Copyright © 2017 Michael Wienands. All rights reserved.
+//  https://github.com/mwienands/SpecialPythagoreanTriplet.git
+
+//A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 //
+//a2 + b2 = c2
+//For example, 32 + 42 = 9 + 16 = 25 = 52.
+//
+//There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+//Find the product abc.
+
+
 
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    
     std::cout << "Hello, World!\n";
     return 0;
 }
